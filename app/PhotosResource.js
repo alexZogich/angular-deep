eaaa42209c162044adfd73e6a28e906f189026e1
alexZogich <alex.zogich@yandex.ru>
2015-07-13 22:44:29 +0300

    angular deep

---
+++ app/PhotosResource.js
@@ -0,0 +1,18 @@
+angular.module('app')
+  .factory('Photos', Photos);
+
+  function Photos ($resource) {
+
+    return {
+      getPhotos: function() {
+        var photos = [];
+
+        $resource('http://jsonplaceholder.typicode.com/photos').query(function (res) {
+          for (prop in res){
+            photos.push(res[prop]);
+            if (res[prop].id == 10) break; 
+        });
+        return photos;
+      }
+    }
+  }
